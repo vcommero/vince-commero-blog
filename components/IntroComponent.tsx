be6e1c650e94610e98c2ccb1eb22a2ea6f457d29
Vince Commero <vacommero@gmail.com>
2025-01-05 00:24:34 -0600

    Added a pic of me!

--- components/IntroComponent.tsx
+++ components/IntroComponent.tsx
@@ -1,12 +1,22 @@
-import { Group, Text } from '@mantine/core'
-import React from 'react'
+import { Group, Image, Text } from "@mantine/core";
+import React from "react";
 
 export default function IntroComponent() {
-  return (
-    <Group justify="center">
-        <Text>
-            {"Hello! I'm Vince Commero and welcome to my corner of the internet! I work as a software engineer and have an avid interest in science and technology. I'm also into cooking, fitness, enjoying the outdoors, and science fiction. This is a space for me to post my thoughts, share things that I'm doing or working on, and share anything else I find interesting."}
-        </Text>
-    </Group>
-  )
+    return (
+        <Group justify="center">
+            <Image
+                src="https://ik.imagekit.io/bhmwwut65/BlogSiteAssets/20221219_100341.png?updatedAt=1736056264988&tr=h-450%2Cw-450"
+                alt="A pic of me!"
+                radius="md"
+                h={250}
+                w="auto"
+                fit="contain"
+            />
+            <Text>
+                {
+                    "Hello! I'm Vince Commero and welcome to my corner of the internet! I work as a software engineer and have an avid interest in science and technology. I'm also into cooking, fitness, enjoying the outdoors, and science fiction. This is a space for me to post my thoughts, share things that I'm doing or working on, and share anything else I find interesting."
+                }
+            </Text>
+        </Group>
+    );
 }
