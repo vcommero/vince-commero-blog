9b344988e55e70ef1bfd3eb380537e0dd471784f
Vince Commero <vacommero@yahoo.com>
2025-04-06 20:52:24 -0500

    Header bar resize.

--- components/layouts/BaseLayout.tsx
+++ components/layouts/BaseLayout.tsx
@@ -66,7 +66,7 @@ export function BaseLayout({ children }: BaseLayoutProps) {
     return (
         <>
             <header className={classes.header}>
-                <Container size="xl" className={classes.inner}>
+                <Container size="1600px" className={classes.inner}>
                     <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
                     <Title order={isMobile ? 3 : 1}>Vince Commero</Title>
                     <Group gap={"lg"} >
